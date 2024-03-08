948a4a2763cc2fda444803fd37ddeca13cb150b1
suvendu021 <suvendukumarsahoo2002@gmail.com>
2024-03-08 11:52:00 +0530

    add cors, cookie-parser and config them

--- src/index.js
+++ src/index.js
@@ -3,9 +3,23 @@
 import dotenv from "dotenv";
 
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.error("express can not interact with DB", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log("server is running at port ", process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.error("MONGODB connection failed", err);
+  });
